3bcb29b52b323005a808cd8c226a2e6dcca9ef1a
Pablo González <pablogonzaleztemes@gmail.com>
2026-05-18 21:13:14 +0200

    feat(items): :sparkles: add constructor and display method

--- src/main/java/edu/teamrocket/items/Item.java
+++ src/main/java/edu/teamrocket/items/Item.java
@@ -1,5 +1,17 @@
 package edu.teamrocket.items;
 
-public class Item {
+public class Item implements Product{
+
+    private final String name;
+    private final double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
     
+    @Override
+    public void display() {
+        System.out.println(this.name + "...." + this.price);
+    }
 }
