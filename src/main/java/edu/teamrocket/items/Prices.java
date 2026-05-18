e8e15fcc238adda7844ebaceda71b2e991f314b8
Pablo González <pablogonzaleztemes@gmail.com>
2026-05-18 21:11:21 +0200

    feat(items): :sparkles: add static methods to add and update extras

--- src/main/java/edu/teamrocket/items/Prices.java
+++ src/main/java/edu/teamrocket/items/Prices.java
@@ -1,5 +1,26 @@
 package edu.teamrocket.items;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Prices {
-    
+
+    private static Map<String, Double> prices = new HashMap<>();
+
+    private Prices() {}
+
+    public static void initPrices() {
+        if (prices.isEmpty()) {
+            prices.putIfAbsent("cheese", 0.25);
+            prices.putIfAbsent("sauce", 0.50);
+            prices.putIfAbsent("medium", 0.25);
+            prices.putIfAbsent("large", 0.50);
+        }
+    }
+
+    public static void display() {
+        for (var extra : prices.entrySet()) {
+            System.out.println(extra.getKey() + "=" + extra.getValue());
+        }
+    }
 }
